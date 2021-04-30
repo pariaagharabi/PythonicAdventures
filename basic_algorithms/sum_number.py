021e80b71bf7f7c1b35f8fac9461af0ed592cb01
PARIA AGHARABI <pariaagharabi@gmail.com>
2021-04-30 11:43:00 -0400

    Adding the method to compute Sum(Number).

---
+++ basic_algorithms/sum_number.py
@@ -0,0 +1,13 @@
+# The method to compute Sum(N) = 1+2+3+…+N.
+
+
+def sum_number(number):
+
+    if number < 1:
+        raise ValueError(
+            "Invalid number, the number should be a positive number!")
+
+    if number > 1:
+        number = number * (number + 1) // 2
+
+    return number
